9e8a12707ad37fa04594805db7ff6d305ad6344a
Juan Camilo Noriega Perlaza <camilo.noriega1@udea.edu.co>
2024-09-07 17:22:58 -0500

    agregar herramientas a inicio de sesion

--- SistemaInformaticoPerinatal/InicioDeSesion.Designer.cs
+++ SistemaInformaticoPerinatal/InicioDeSesion.Designer.cs
@@ -28,12 +28,88 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "InicioDeSesion";
+            btnIniciar = new Button();
+            lblUsuario = new Label();
+            txtUsuario = new TextBox();
+            txtContraseña = new TextBox();
+            btnSalir = new Button();
+            lblContraseña = new Label();
+            SuspendLayout();
+            // 
+            // btnIniciar
+            // 
+            btnIniciar.Location = new Point(220, 324);
+            btnIniciar.Name = "btnIniciar";
+            btnIniciar.Size = new Size(75, 23);
+            btnIniciar.TabIndex = 0;
+            btnIniciar.Text = "INICIAR";
+            btnIniciar.UseVisualStyleBackColor = true;
+            // 
+            // lblUsuario
+            // 
+            lblUsuario.AutoSize = true;
+            lblUsuario.Location = new Point(98, 120);
+            lblUsuario.Name = "lblUsuario";
+            lblUsuario.Size = new Size(56, 15);
+            lblUsuario.TabIndex = 1;
+            lblUsuario.Text = "USUARIO";
+            // 
+            // txtUsuario
+            // 
+            txtUsuario.Location = new Point(187, 112);
+            txtUsuario.Name = "txtUsuario";
+            txtUsuario.Size = new Size(100, 23);
+            txtUsuario.TabIndex = 2;
+            // 
+            // txtContraseña
+            // 
+            txtContraseña.Location = new Point(187, 223);
+            txtContraseña.Name = "txtContraseña";
+            txtContraseña.Size = new Size(100, 23);
+            txtContraseña.TabIndex = 3;
+            // 
+            // btnSalir
+            // 
+            btnSalir.Location = new Point(79, 324);
+            btnSalir.Name = "btnSalir";
+            btnSalir.Size = new Size(75, 23);
+            btnSalir.TabIndex = 4;
+            btnSalir.Text = "SALIR";
+            btnSalir.UseVisualStyleBackColor = true;
+            // 
+            // lblContraseña
+            // 
+            lblContraseña.AutoSize = true;
+            lblContraseña.Location = new Point(71, 231);
+            lblContraseña.Name = "lblContraseña";
+            lblContraseña.Size = new Size(83, 15);
+            lblContraseña.TabIndex = 5;
+            lblContraseña.Text = "CONTRASEÑA";
+            // 
+            // InicioDeSesion
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(389, 387);
+            Controls.Add(lblContraseña);
+            Controls.Add(btnSalir);
+            Controls.Add(txtContraseña);
+            Controls.Add(txtUsuario);
+            Controls.Add(lblUsuario);
+            Controls.Add(btnIniciar);
+            Name = "InicioDeSesion";
+            Text = "InicioDeSesion";
+            ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
+
+        private Button btnIniciar;
+        private Label lblUsuario;
+        private TextBox txtUsuario;
+        private TextBox txtContraseña;
+        private Button btnSalir;
+        private Label lblContraseña;
     }
 }
