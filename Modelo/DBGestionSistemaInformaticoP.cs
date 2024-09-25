1ab5678ec6827ed1e3abaa16dd051fa247a4fd39
Juan Camilo Noriega Perlaza <camilo.noriega1@udea.edu.co>
2024-09-25 16:50:44 -0500

    swjguwbdad

--- Modelo/DBGestionSistemaInformaticoP.cs
+++ Modelo/DBGestionSistemaInformaticoP.cs
@@ -5,12 +5,29 @@ namespace Modelo
 {
     public class DBGestionSistemaInformaticoP : DbContext
     {
-        public DbSet<EstadoCivil> Civiles { get; set;}
+        public DbSet<EstadoCivil> EstadoCivil { get; set;}
+        public DbSet<Etnia> Etina  { get; set; }
 
 
         protected override void OnConfiguring(DbContextOptionsBuilder builder)
         {
-            builder.UseSqlServer(ConfigurationManager.ConnectionStrings["GestionActividadFisica"].ConnectionString);
+            builder.UseSqlServer(ConfigurationManager.ConnectionStrings["SIPerinatal"].ConnectionString);
+        }
+
+        protected override void OnModelCreating(ModelBuilder Builder)
+        {
+            base.OnModelCreating(Builder);
+
+
+            Builder.Entity<EstadoCivil>()
+                .HasMany(e => e.Paciente)
+                .WithOne(e => e.EstadoCivil)
+                .HasForeignKey(e => e.IdEstadoCivil);
+
+            Builder.Entity<Etnia>()
+                .HasMany(e => e.Paciente)
+                .WithOne(e => e.Etnia)
+                .HasForeignKey(e => e.IdEtnia);
         }
 
     }
